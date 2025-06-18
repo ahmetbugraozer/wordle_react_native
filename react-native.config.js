f9331e6755aeefa7952556624dc08f594189b9f4
Ahmet Buğra <its4thaxeon@outlook.com>
2025-06-18 06:57:20 +0300

    made fixes

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dependencies: {
+    '@react-native-async-storage/async-storage': {
+      platforms: {
+        android: {
+          sourceDir: '../node_modules/@react-native-async-storage/async-storage/android',
+          packageImportPath: 'io.github.reactnativecommunity.asyncstorage',
+        },
+      },
+    },
+  },
+};
